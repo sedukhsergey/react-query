1c97ec4f7e5b2f3e3cad125333a5b3be5af66f55
sedykh-sergey <sedukhsergey@gmail.com>
2021-07-12 23:58:55 +0300

    code refactoring

--- src/components/Todos/hooks/useAddTodo.js
+++ src/components/Todos/hooks/useAddTodo.js
@@ -4,15 +4,13 @@ import {
 import { createTodo } from '../../../api/todos';
 
 export const useAddTodo = () => {
-  const queryClient = useQueryClient();
+const queryClient = useQueryClient();
   return useMutation(createTodo, {
     onSuccess: (response, variables) => {
       const cachedTodos = queryClient.getQueryData('todos');
       queryClient.setQueryData('todos', [...cachedTodos, {
         id: response.id, title: variables.title,
       }]);
-      return Promise.resolve();
     },
-    onError: () => Promise.reject(),
   });
 };
